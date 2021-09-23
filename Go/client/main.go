a2e7e0a2cb47ec4085101876d327d58b1f7b26ad
BuddhiWathsala <b.wathsala.bw@gmail.com>
2021-09-23 12:09:59 +0530

    Add Go client implementation

---
+++ Go/client/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+
+	pb "github.com/BuddhiWathsala/grpc-compression/Go/client/stubs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/encoding/gzip"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+const (
+	address     = "localhost:50052"
+	defaultName = "world"
+)
+
+func main() {
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewHelloWorldServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	maxReqSizeOption := grpc.MaxCallRecvMsgSize(6455878)
+	maxResSizeOption := grpc.MaxCallSendMsgSize(6455878)
+	compressor := grpc.UseCompressor(gzip.Name)
+	r, err := c.Hello(ctx, &wrapperspb.StringValue{Value: "Hello"}, maxReqSizeOption, maxResSizeOption, compressor)
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r)
+}
