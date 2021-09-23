18146e67dd47c68be2921b65f601c738d59065f6
BuddhiWathsala <b.wathsala.bw@gmail.com>
2021-09-23 12:10:13 +0530

    Add Go server implementation

---
+++ Go/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	_ "google.golang.org/grpc/encoding/gzip"
+
+	hp "github.com/BuddhiWathsala/grpc-compression/Go/server/stubs"
+	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct {
+	hp.UnimplementedHelloWorldServiceServer
+}
+
+func (s *server) Hello(ctx context.Context, in *wrapperspb.StringValue) (*wrapperspb.StringValue, error) {
+	dat, _ := os.ReadFile("../../resources/text_file.txt")
+	return &wrapperspb.StringValue{Value: string(dat)}, nil
+}
+
+func main() {
+	s := grpc.NewServer()
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	hp.RegisterHelloWorldServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
